6760bbc447a7526a8d68b394d775fe9aeb67c628
ScorchedPsyche <scorchedpsychegames@gmail.com>
2020-08-19 04:18:07 -0300

    Attempt to synchronize nearby players with packets.
    Not working...

--- src/main/java/com/github/scorchedpsyche/craftera_suite/entities/baby/listeners/EntityNamingListener.java
+++ src/main/java/com/github/scorchedpsyche/craftera_suite/entities/baby/listeners/EntityNamingListener.java
@@ -1,7 +1,14 @@
 package com.github.scorchedpsyche.craftera_suite.entities.baby.listeners;
 
 import com.github.scorchedpsyche.craftera_suite.entities.baby.utils.EntityUtil;
+import net.minecraft.server.v1_16_R2.DataWatcher;
+import net.minecraft.server.v1_16_R2.DataWatcherRegistry;
+import net.minecraft.server.v1_16_R2.PacketPlayOutEntityMetadata;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftAgeable;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftPlayer;
 import org.bukkit.entity.Ageable;
 import org.bukkit.entity.Breedable;
 import org.bukkit.entity.Entity;
@@ -12,6 +19,7 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class EntityNamingListener implements Listener {
+    private String Text_CraftEra_Suite = ChatColor.AQUA + "" + ChatColor.BOLD + "[CraftEra Suite] " + ChatColor.RESET;
     /**
      * Listens to player right-click interaction and checks if the target entity is renamed to "ces_baby/adult".
      * TO DO
@@ -52,23 +60,22 @@ public class EntityNamingListener implements Listener {
                         if( ((Ageable) targetEntity).isAdult() ){
                             // Successfully converted to Adult
                             sourcePlayer.sendRawMessage(
-                                    ChatColor.AQUA + "" + ChatColor.BOLD + "[CraftEra Suite] " + ChatColor.RESET +
-                                            ChatColor.GREEN + targetEntity.getName() + ChatColor.RESET +
+                                    Text_CraftEra_Suite + ChatColor.GREEN + targetEntity.getName() + ChatColor.RESET +
                                             " is now an adult!" );
+
+//                                DataWatcher watcher = new DataWatcher(targetEntity);
+//                                watcher.a(15, true); // 15 = isBaby
                         } else {
                             // Failed. Can't be an adult
                             sourcePlayer.sendRawMessage(
-                                    ChatColor.AQUA + "" + ChatColor.BOLD + "[CraftEra Suite] " + ChatColor.RESET +
-                                            ChatColor.RED + targetEntity.getName() + ChatColor.RESET +
+                                    Text_CraftEra_Suite + ChatColor.RED + targetEntity.getName() + ChatColor.RESET +
                                             " can't be an adult." );
                         }
                     } else {
                         // Hasn't been converted so the conversion is invalid.
                         sourcePlayer.sendRawMessage(
-                                ChatColor.AQUA + "" + ChatColor.BOLD + "[CraftEra Suite] " + ChatColor.RESET +
-                                        ChatColor.RED + "This is a natural baby!" + ChatColor.RESET + " You can't " +
-                                        "convert Vanilla " +
-                                        "entities to Adult.");
+                                Text_CraftEra_Suite + ChatColor.RED + "This is a natural baby!" + ChatColor.RESET +
+                                        " You can't convert Vanilla entities to Adult.");
                     }
                 } else {
                     // Convert to Baby
@@ -80,25 +87,35 @@ public class EntityNamingListener implements Listener {
                     if( !((Ageable) targetEntity).isAdult() ){
                         // Successfully converted to Baby
                         sourcePlayer.sendRawMessage(
-                                ChatColor.AQUA + "" + ChatColor.BOLD + "[CraftEra Suite] " + ChatColor.RESET +
-                                        ChatColor.GREEN + targetEntity.getName() + ChatColor.RESET +
+                                Text_CraftEra_Suite + ChatColor.GREEN + targetEntity.getName() + ChatColor.RESET +
                                         " is now a baby!" );
                     } else {
                         // Failed. Can't be a baby
                         ((Breedable) targetEntity).setAgeLock(false);
 
                         sourcePlayer.sendRawMessage(
-                                ChatColor.AQUA + "" + ChatColor.BOLD + "[CraftEra Suite] " + ChatColor.RESET +
-                                        ChatColor.RED + targetEntity.getName() + ChatColor.RESET +
+                                Text_CraftEra_Suite + ChatColor.RED + targetEntity.getName() + ChatColor.RESET +
                                         " can't be a baby." );
                     }
                 }
             } else {
                 // Invalid entity for conversion
                 sourcePlayer.sendRawMessage(
-                        ChatColor.AQUA + "" + ChatColor.BOLD + "[CraftEra Suite] " +
-                                ChatColor.RESET + ChatColor.GREEN + targetEntity.getName() +
-                                ChatColor.RESET + " is not valid for conversion!" );
+                        Text_CraftEra_Suite + ChatColor.GREEN + targetEntity.getName() + ChatColor.RESET +
+                                " is not valid for conversion!" );
+            }
+
+            DataWatcher watcher = ((CraftAgeable) targetEntity).getHandle().getDataWatcher();
+
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                PacketPlayOutEntityMetadata packet = new PacketPlayOutEntityMetadata(
+                        targetEntity.getEntityId(), // Entity ID
+                        watcher, // Data watcher which you can get by accessing a method in a NMS Entity class
+                        false // Send All
+                );
+                
+                ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+                sourcePlayer.sendRawMessage("SENT: " + player.getName() );
             }
         }
     }
